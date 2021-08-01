99d9be2dd09b40851e5d8c98b4a5d6c41de4397d
Bhis10 <bhisma.mgr@gmail.com>
2021-08-01 16:22:37 +0100

    carousel file change

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 $(document).ready(function(){
-    $('.slider').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+  $('.owl-carousel').owlCarousel({
+      responsiveBaseElement: 'body',
+      responsiveClass: true,
+      loop: true,
+      margin: 10,
+      nav: true,
+      autoplay: false,
+      autoplayTimeout: 5000,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
         }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  })
-});
+     }
+    });
+  });
